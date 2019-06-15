38068cb94bf7aae7477170ee7a5b5bd2adef3921
Jordan Hansen <jbhansen84@gmail.com>
2019-06-15 04:48:16 -0600

    clean up the code and make it do it right

--- src/findDeadLinks.ts
+++ src/findDeadLinks.ts
@@ -18,61 +18,31 @@ export async function findDeadLinks(domain: string) {
         throw `Error requesting base domain - ${domain}, ${e.statusCode}`;
     }
     let links: ILinkObject[] = await getLinks(html, domain, domain);
-    // const checkLink = await spawn(new Worker('./../../../../dist/checkLinkWorker.js'));
 
     const spawnLinkChecker = () => {
         return spawn(new Worker('./../../../../dist/checkLinkWorker.js'));
     }
-    const pool = Pool(spawnLinkChecker, 4);
+    const pool = Pool(spawnLinkChecker, 6);
     for (let i = 0; i < links.length; i++) {
         if (!links[i].status) {
-            const promises: any[] = [];
-
-            // const amountOfThreads = 20;
-            // for (let linkToCheckIndex = 0; linkToCheckIndex < amountOfThreads; linkToCheckIndex++) {
-            //     if (links[i + linkToCheckIndex]) {
-            //         promises.push(checkLink(links[i + linkToCheckIndex], domain));
-            //     }
-            // }
-
-
             pool.queue(linkChecker => linkChecker(links[i], domain));
-
-            // const checkLinkResponses = await Promise.all(promises);
-
-            // for (let index = 0; index < checkLinkResponses.length; index++) {
-            //     // Replace the link that doesn't have a status with the link that does
-            //     let linkToReplaceIndex = links.findIndex(linkObject => linkObject.link === checkLinkResponses[index].link.link);
-            //     links[linkToReplaceIndex] = checkLinkResponses[index].link;
-
-            //     // This part needs to check for duplicate links
-            //     // So we can't do it concurrently just in case we miss duplicates
-            //     for (let linkToCheck of checkLinkResponses[index].links) {
-            //         if (links.filter(linkObject => linkObject.link === linkToCheck.link).length < 1) {
-            //             console.log('pushed in ', linkToCheck.link);
-            //             links.push(linkToCheck);
-            //         }
-            //     }
-            // }
-            // i += amountOfThreads - 1;
-
-            // console.log('after link is checked link', links[i], i);
-            // console.log('current links length and current index ***************', links.length, i);
-
         }
     }
 
     pool.events().subscribe((event) => {
-        // console.log('pool event', event);
         if (event.type === 'taskCompleted' && event.returnValue.links) {
             console.log('task completed', new Date());
+
+            // Replace the link we were checking with the completed object
+            let linkToReplaceIndex = links.findIndex(linkObject => linkObject.link === event.returnValue.link.link);
+            links[linkToReplaceIndex] = event.returnValue.link;
+
             for (let linkToCheck of event.returnValue.links) {
                 if (links.filter(linkObject => linkObject.link === linkToCheck.link).length < 1) {
                     console.log('pushed in ', linkToCheck.link);
-                    // links.push(linkToCheck);
+                    links.push(linkToCheck);
 
-                    pool.queue(linkChecker => linkChecker(linkToCheck.link, domain));
-
+                    pool.queue(linkChecker => linkChecker(linkToCheck, domain));
                 }
             }
         }
